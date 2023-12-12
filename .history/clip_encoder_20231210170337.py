2a38f95e731087be1eb9824a68c7397e331c7c86
zzw <565998373@qq.com>
2023-12-12 21:22:29 +0800

    first commit

---
+++ .history/clip_encoder_20231210170337.py
@@ -0,0 +1,21 @@
+import torch
+import clip
+from PIL import Image
+
+print(clip.available_models())
+# ['RN50', 'RN101', 'RN50x4', 'RN50x16', 'RN50x64', 'ViT-B/32', 'ViT-B/16', 'ViT-L/14', 'ViT-L/14@336px']
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+
+image = preprocess(Image.open("CLIP-main/CLIP.png")).unsqueeze(0).to(device)
+text = clip.tokenize(["a diagram", "a dog", "a cat"]).to(device)
+
+print(image.shape, text.shape)
+with torch.no_grad():
+    image_features = model.encode_image(image)
+    text_features = model.encode_text(text)
+    
+    logits_per_image, logits_per_text = model(image, text)
+    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+
+print("Label probs:", probs)  # prints: [[0.9927937  0.00421068 0.00299572]]
---
+++ .history/coop_encoder_20231210150821.py
@@ -0,0 +1,15 @@
+from dassl.utils import setup_logger, set_random_seed, collect_env_info
+from dassl.config import get_cfg_default
+from dassl.engine import build_trainer
+
+from COOP.trainers import coop
+
+def build_coop_encoder(args)
+
+    cfg = setup_cfg(args)
+        if cfg.SEED >= 0:
+            print("Setting fixed seed: {}".format(cfg.SEED))
+            set_random_seed(cfg.SEED)
+        setup_logger(cfg.OUTPUT_DIR)
+    
+    model = coop.CustomCLIP(cfg, classnames=, clip_model=)
